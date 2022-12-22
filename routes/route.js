15e7f4aca76963617e8fb6a8dd12de3d6a9e8f41
dzeep <fatihhevesli@gmail.com>
2022-12-22 15:28:54 +0300

    paths fixed

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../controllers/route.js');
+const controller = require('./controllers/route.js');
 
 /**
  *  @description Root Route
