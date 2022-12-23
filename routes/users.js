88d7e1cdac6d3a11adb7af22e4bf4fc291d972e9
dzeep <fatihhevesli@gmail.com>
2022-12-23 17:21:41 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('controllers/users.js');
+const controller = require('../controllers/users.js');
 
 router.post('/register', controller.postregister);
 router.post('/login', controller.postuser);
