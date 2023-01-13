de357854f423bdf1994a5218f8cd8eda4b9ddc30
Yahya Bekir Canevi <yahyabekircanevi2@gmail.com>
2023-01-13 16:23:04 +0300

    Socket removed from express app to its own adress

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
-const path = require('path');
 const router = express.Router();
-const { auth, socketAuth } = require("./security/middleware/auth");
+const { auth } = require("./security/middleware/auth");
 const swaggerUI = require("swagger-ui-express");
 const docs = require('./docs');
 //const swaggerFile = require('../swagger-output.json')
@@ -32,34 +31,17 @@ app.use('/api/post', require('./routes/posts.js'), auth);
 
 app.use('/api/route', require('./routes/route.js'), auth);
 
-/* router.get('/', function (req, res) {
+router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
   //__dirname : It will resolve to your project folder.
-}); */
+});
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
 //app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 const PORT = process.env.PORT || 4652;
-const SOCKETIO_PORT = process.env.SOCKETIO_PORT || 5652;
 app.use('/', router);
 
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
-
-const server = require('http').Server(app);
-const socketio = require('socket.io')(server, {
-  path: "/my-socket"
-});
-const { Client } = require('./websocket/client.js')
-
-socketio
-  .use(socketAuth)
-  .on('connection', (client) => {
-    console.log(`Socket connected: ${client.id}`)
-    Client(socketio, client)
-  })
-server.listen(SOCKETIO_PORT, () =>
-  console.log(`Socket.io Server running in ${process.env.NODE_ENV} mode on port ${SOCKETIO_PORT}`)
 )
